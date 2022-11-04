7b81845001600ca9e3b7f4911e037c69fcdf1f4f
Felipe0408 <felipe.castillo@utp.ac.pa>
2022-11-04 00:11:41 -0600

    Commit de la automatización
    En este commit están todos los requermientos pedidos en el lab como buscar la página de Amazon, agregar al carrito el primer item, etc.

--- prueba_1.py
+++ prueba_1.py
@@ -1,11 +1,34 @@
+from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 import time
 
 driver = webdriver.Chrome('C:/Users/50768/Documents/GitHub/Lab9/chromedriver.exe')
 driver.maximize_window()
 driver.delete_all_cookies()
 driver.get("https://www.amazon.com/")
+
+#Busca la barra de busqueda, ingres el texto "HP Pavilon azul", y le da submit.
+Busqueda = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "twotabsearchtextbox")))
+time.sleep(5)
+TextoBusqueda = ("HP Pavilon azul")
+Busqueda.send_keys(TextoBusqueda)
+time.sleep(5)
+Busqueda.submit()
+
+#Busca el primer elemento, le da click y luego
+primer_elemento = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.LINK_TEXT, "HP Pavilion - Laptop IPS FHD de 14 pulgadas (modelo 2022), Intel Core i3-1125G4 (Beats i7-1065G7), 16GB RAM, SSD de 512GB, lector de huellas dactilares, batería de larga duración, cámara web, HDMI, WiFi, Bluetooth, Win10")))
+primer_elemento.click()
+time.sleep(5)
+cantidad = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "quantity")))
+cantidad.send_keys("2")
+time.sleep(5)
+Agregar_a_carrito = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "add-to-cart-button")))
+Agregar_a_carrito.click()
+time.sleep(3)
+ir_a_carrito = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.LINK_TEXT, "Ir al Carrito")))
+ir_a_carrito.click()
 
 time.sleep(100)
 driver.close()
