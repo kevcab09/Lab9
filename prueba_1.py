70c19795e944df3fb67d86aed92c698627638bfe
Felipe0408 <felipe.castillo@utp.ac.pa>
2022-11-03 23:37:32 -0600

    Commit de los primeros psos del lab
    Este commit contiene los cambios de la primera parte

--- prueba_1.py
+++ prueba_1.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-driver = webdriver.Chrome('C:/Selenium/chromedriver')
+driver = webdriver.Chrome('C:/Users/50768/Documents/GitHub/Lab9/chromedriver.exe')
 driver.maximize_window()
 driver.delete_all_cookies()
 driver.get("https://www.amazon.com/")
